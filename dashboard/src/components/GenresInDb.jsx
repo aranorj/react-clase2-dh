970d44cf44ab36ff77bff83b3b8686f576a7bc29
aranorj <arano.rocio.b@gmail.com>
2024-03-11 21:44:54 -0300

    feat: consumo de api de generos

--- dashboard/src/components/GenresInDb.jsx
+++ dashboard/src/components/GenresInDb.jsx
@@ -3,15 +3,22 @@ import Generos from "./Generos";
 
 class GenresInDb extends Component{  
 
-    
+
 
     render(){
         return(
-            <>
-            <h1>HOLA</h1>
-            <Generos/>
-
-            </>
+            
+        <section className="content">
+             <h2 className="mt-3">Géneros</h2>
+            <div className="list-group shadow-sm p-3 mb-5 bg-body-tertiary rounded">
+            <button type="button" className="list-group-item list-group-item-action active text-center"
+            aria-current="true">
+            Listado de géneros de las películas
+            </button>
+            {this.state.genresList.map((genero)=> <Generos genero={genero} key={genero.id}/>)}
+            </div>
+            </section>
+          
         )
     }
 
@@ -20,6 +27,12 @@ class GenresInDb extends Component{
         this.state = {
             genresList: []
         }
+    }
+
+    componentDidMount(){
+        fetch("http://localhost:3030/genres")
+        .then((respuesta)=> respuesta.json())
+        .then((generos)=>this.setState({genresList: generos}))
     }
 
 }
